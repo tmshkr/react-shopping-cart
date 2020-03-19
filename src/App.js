82549ade653442ec74dfa93c3cb7875007b206c7
Tim Shaker <tim@tmshkr.com>
2020-03-19 11:57:03 -0700

    useLocalStorage hook

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 import { Route } from "react-router-dom";
-import data from "./data";
+import products from "./data";
 
 // Components
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
 
+import { useLocalStorage } from "./hooks/useLocalStorage";
+
 import { ProductContext } from "./contexts/ProductContext";
 import { CartContext } from "./contexts/CartContext";
 
 function App() {
-  const [products] = useState(data);
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useLocalStorage("cart", []);
 
   const addItem = item => {
     setCart([...cart, item]);
