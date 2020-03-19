45ec45c3c90b66dbb2c2031a1fd390c8a028f877
Tim Shaker <tim@tmshkr.com>
2020-03-19 11:41:40 -0700

    ProductContext consumers

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
 
+import { ProductContext } from "./contexts/ProductContext";
+
 function App() {
   const [products] = useState(data);
   const [cart, setCart] = useState([]);
@@ -16,18 +18,20 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <Navigation cart={cart} />
+    <ProductContext.Provider value={{ products, addItem }}>
+      <div className="App">
+        <Navigation cart={cart} />
 
-      {/* Routes */}
-      <Route exact path="/">
-        <Products products={products} addItem={addItem} />
-      </Route>
+        {/* Routes */}
+        <Route exact path="/">
+          <Products />
+        </Route>
 
-      <Route path="/cart">
-        <ShoppingCart cart={cart} />
-      </Route>
-    </div>
+        <Route path="/cart">
+          <ShoppingCart cart={cart} />
+        </Route>
+      </div>
+    </ProductContext.Provider>
   );
 }
 
